1b4ffa638f0a3119bc0c5fabf5455062d8b45140
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-07-30 23:37:49 +0300

    Application test updates

--- onlineradiosearchmobile/test/widget_test.dart
+++ onlineradiosearchmobile/test/widget_test.dart
@@ -11,20 +11,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:onlineradiosearchmobile/main.dart';
 
 void main() {
-//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
-//    await tester.pumpWidget(MyApp());
-//
-//    // Verify that our counter starts at 0.
-//    expect(find.text('0'), findsOneWidget);
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(OnlineRadioSearchApp());
+
+    var a = tester.allElements;
+    // Verify that our counter starts at 0.
+    expect(find.widgetWithText(AppBar ,'Popular stations'), findsOneWidget);
+    expect(find.text('Loading...'), findsOneWidget);
 //    expect(find.text('1'), findsNothing);
-//
-//    // Tap the '+' icon and trigger a frame.
+
+    // Tap the '+' icon and trigger a frame.
 //    await tester.tap(find.byIcon(Icons.add));
 //    await tester.pump();
-//
-//    // Verify that our counter has incremented.
+
+    // Verify that our counter has incremented.
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
-//  });
+  });
 }
