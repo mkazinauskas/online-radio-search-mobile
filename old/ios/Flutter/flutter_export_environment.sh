51fb17a9cba19c40b1d2fcd5a91cf8ed4b903815
Modestas <m.kazinauskas@gmail.com>
2020-06-17 00:48:30 +0300

    New flutter version

---
+++ old/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/modestas/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/modestas/dev/online-radio-search-mobile"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/modestas/tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=2.0.0"
