42e7a10cd1affdb5cb2b179bc92a1a8d35b42841
Modestas <m.kazinauskas@gmail.com>
2020-06-18 22:36:30 +0300

    Working radio split to classes

---
+++ lib/MainScreen.dart
@@ -0,0 +1,185 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter/material.dart';
+import 'package:onlineradiosearchmobile/AudioPlayerTask.dart';
+import 'package:onlineradiosearchmobile/ScreenState.dart';
+import 'package:rxdart/rxdart.dart';
+
+// NOTE: Your entrypoint MUST be a top-level function.
+void _audioPlayerTaskEntrypoint() async {
+  AudioServiceBackground.run(() => AudioPlayerTask());
+}
+
+class MainScreen extends StatelessWidget {
+  /// Tracks the position while the user drags the seek bar.
+  final BehaviorSubject<double> _dragPositionSubject =
+  BehaviorSubject.seeded(null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio Service Demo'),
+      ),
+      body: Center(
+        child: StreamBuilder<ScreenState>(
+          stream: _screenStateStream,
+          builder: (context, snapshot) {
+            final screenState = snapshot.data;
+            final queue = screenState?.queue;
+            final mediaItem = screenState?.mediaItem;
+            final state = screenState?.playbackState;
+            final processingState =
+                state?.processingState ?? AudioProcessingState.none;
+            final playing = state?.playing ?? false;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (processingState == AudioProcessingState.none) ...[
+                  audioPlayerButton(),
+                ] else ...[
+                  if (queue != null && queue.isNotEmpty)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.skip_previous),
+                          iconSize: 64.0,
+                          onPressed: mediaItem == queue.first
+                              ? null
+                              : AudioService.skipToPrevious,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.skip_next),
+                          iconSize: 64.0,
+                          onPressed: mediaItem == queue.last
+                              ? null
+                              : AudioService.skipToNext,
+                        ),
+                      ],
+                    ),
+                  if (mediaItem?.title != null) Text(mediaItem.title),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (playing) pauseButton() else playButton(),
+                      stopButton(),
+                    ],
+                  ),
+                  positionIndicator(mediaItem, state),
+                  Text("Processing state: " +
+                      "$processingState".replaceAll(RegExp(r'^.*\.'), '')),
+                  StreamBuilder(
+                    stream: AudioService.customEventStream,
+                    builder: (context, snapshot) {
+                      return Text("custom event: ${snapshot.data}");
+                    },
+                  ),
+                  StreamBuilder<bool>(
+                    stream: AudioService.notificationClickEventStream,
+                    builder: (context, snapshot) {
+                      return Text(
+                        'Notification Click Status: ${snapshot.data}',
+                      );
+                    },
+                  ),
+                ],
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  /// Encapsulate all the different data we're interested in into a single
+  /// stream so we don't have to nest StreamBuilders.
+  Stream<ScreenState> get _screenStateStream =>
+      Rx.combineLatest3<List<MediaItem>, MediaItem, PlaybackState, ScreenState>(
+          AudioService.queueStream,
+          AudioService.currentMediaItemStream,
+          AudioService.playbackStateStream,
+              (queue, mediaItem, playbackState) =>
+              ScreenState(queue, mediaItem, playbackState));
+
+  RaisedButton audioPlayerButton() => startButton(
+    'AudioPlayer',
+        () {
+      AudioService.start(
+        backgroundTaskEntrypoint: _audioPlayerTaskEntrypoint,
+        androidNotificationChannelName: 'Audio Service Demo',
+        // Enable this if you want the Android service to exit the foreground state on pause.
+        //androidStopForegroundOnPause: true,
+        androidNotificationColor: 0xFF2196f3,
+        androidNotificationIcon: 'mipmap/ic_launcher',
+        androidEnableQueue: true,
+      );
+    },
+  );
+
+  RaisedButton startButton(String label, VoidCallback onPressed) =>
+      RaisedButton(
+        child: Text(label),
+        onPressed: onPressed,
+      );
+
+  IconButton playButton() => IconButton(
+    icon: Icon(Icons.play_arrow),
+    iconSize: 64.0,
+    onPressed: AudioService.play,
+  );
+
+  IconButton pauseButton() => IconButton(
+    icon: Icon(Icons.pause),
+    iconSize: 64.0,
+    onPressed: AudioService.pause,
+  );
+
+  IconButton stopButton() => IconButton(
+    icon: Icon(Icons.stop),
+    iconSize: 64.0,
+    onPressed: AudioService.stop,
+  );
+
+  Widget positionIndicator(MediaItem mediaItem, PlaybackState state) {
+    double seekPos;
+    return StreamBuilder(
+      stream: Rx.combineLatest2<double, double, double>(
+          _dragPositionSubject.stream,
+          Stream.periodic(Duration(milliseconds: 200)),
+              (dragPosition, _) => dragPosition),
+      builder: (context, snapshot) {
+        double position =
+            snapshot.data ?? state.currentPosition.inMilliseconds.toDouble();
+        double duration = mediaItem?.duration?.inMilliseconds?.toDouble();
+        return Column(
+          children: [
+            if (duration != null)
+              Slider(
+                min: 0.0,
+                max: duration,
+                value: seekPos ?? max(0.0, min(position, duration)),
+                onChanged: (value) {
+                  _dragPositionSubject.add(value);
+                },
+                onChangeEnd: (value) {
+                  AudioService.seekTo(Duration(milliseconds: value.toInt()));
+                  // Due to a delay in platform channel communication, there is
+                  // a brief moment after releasing the Slider thumb before the
+                  // new position is broadcast from the platform side. This
+                  // hack is to hold onto seekPos until the next state update
+                  // comes through.
+                  // TODO: Improve this code.
+                  seekPos = value;
+                  _dragPositionSubject.add(null);
+                },
+              ),
+            Text("${state.currentPosition}"),
+          ],
+        );
+      },
+    );
+  }
+}
