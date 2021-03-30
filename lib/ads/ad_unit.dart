bfc8eff9528b3a694d1f01acafc115b3e2e1e8ac
Modestas <m.kazinauskas@gmail.com>
2021-03-30 10:37:15 +0300

    New ad units

--- lib/ads/ad_unit.dart
+++ lib/ads/ad_unit.dart
@@ -1,21 +1,22 @@
 class AdUnit {
+
   static final AdUnit favouritesBannerTop =
       new AdUnit._('ca-app-pub-4421922308722660/9905247705');
 
   static final AdUnit favouritesBannerBottom =
-      new AdUnit._('ca-app-pub-4421922308722660/9905247705');
+      new AdUnit._('ca-app-pub-4421922308722660~3018260097');
 
   static final AdUnit playerScreenMiddle =
-      new AdUnit._('ca-app-pub-4421922308722660/9905247705');
+      new AdUnit._('ca-app-pub-4421922308722660/5347797854');
 
   static final AdUnit searchScreenTop =
-      new AdUnit._('ca-app-pub-4421922308722660/9905247705');
+      new AdUnit._('ca-app-pub-4421922308722660/2050538916');
 
   static final AdUnit discoveryScreenTop =
-      new AdUnit._('ca-app-pub-4421922308722660/9905247705');
+      new AdUnit._('ca-app-pub-4421922308722660/3317365031');
 
   static final AdUnit discoveryScreenBottom =
-      new AdUnit._('ca-app-pub-4421922308722660/9905247705');
+      new AdUnit._('ca-app-pub-4421922308722660/2721634512');
 
   final String id;
 
